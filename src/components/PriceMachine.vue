bc43f95f3aae22aee92842dae70f650c1c8b505f
BOB <1971785019@qq.com>
2018-09-06 18:09:16 +0800

    wechat

---
+++ src/components/PriceMachine.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'PriceMachine'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Router from 'vue-router'
 import CikeBaidu from '@/pages/CikeBaidu'
 import Business from '@/pages/Business'
 import Travel from '@/pages/Travel'
+import WeChat from '@/pages/WeChat'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path: '/', name: 'CikeBaidu', component: CikeBaidu},
     {path: '/business', name: 'business', component: Business},
-    {path: '/travel', name: 'travel', component: Travel}
+    {path: '/travel', name: 'travel', component: Travel},
+    {path: '/WeChat', name: 'WeChat', component: WeChat}
   ]
 })
