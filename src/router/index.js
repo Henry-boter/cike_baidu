b289b93f486a7876d852a7d0137106c2581fc059
hebo <1971785019@qq.com>
2019-04-01 10:15:42 +0800

    删除不必要的文件

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,10 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path: '/', name: 'CikeBaidu', component: resolve => require(['../pages/CikeBaidu'], resolve)},
-    {path: '/business', name: 'business', component: resolve => require(['../pages/Business'], resolve)},
-    {path: '/travel', name: 'travel', component: resolve => require(['../pages/Travel'], resolve)},
-    {path: '/WeChat', name: 'WeChat', component: resolve => require(['../pages/WeChat'], resolve)},
-    {path: '/news', name: 'news', component: resolve => require(['../pages/news'], resolve)}
+    {path: '/', name: 'CikeBaidu', component: resolve => require(['../pages/CikeBaidu'], resolve)}, // 主页
+    {path: '/business', name: 'business', component: resolve => require(['../pages/Business'], resolve)}, // 商务学习
+    {path: '/travel', name: 'travel', component: resolve => require(['../pages/Travel'], resolve)}, // 出国旅游
+    {path: '/WeChat', name: 'WeChat', component: resolve => require(['../pages/WeChat'], resolve)}, // 微信推广
+    {path: '/news', name: 'news', component: resolve => require(['../pages/news'], resolve)} // 用于腾讯新闻
   ]
 })
